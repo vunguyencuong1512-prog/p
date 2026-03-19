2e81f93d089ef5af97c18e82e8e33a4b0f70cf4a
Cuong <vunguyencuong1512@gmail.com>
2026-03-19 15:16:39 +0700

    Add APIView

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home'),
+    path('',views.HelloApiView.as_view()),
     
     
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse , HttpRequest
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response 
 
-def home(request):
-    return render(request,'base.html')
-
+class HelloApiView(APIView):
+    # First API view 
+    def get(self,request,format=None):
+        # Returns a list of APIView Features
+        a_apiview = [
+            'Uses HTTP methods as functions (get,post,patch,delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your application logic',
+            'Is mapped manually to URLs',
+        ] 
+        return Response({'message':'Hello!','a_apiview':a_apiview})
+        
+        
+    
